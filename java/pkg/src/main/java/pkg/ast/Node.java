13d015d791cf242aad7ab466a26e8e94b84dc1db
Kelechi Ukpabi <kuolingou@gmail.com>
2024-09-30 09:34:38 -0400

    Add comments

--- java/pkg/src/main/java/pkg/ast/Node.java
+++ java/pkg/src/main/java/pkg/ast/Node.java
@@ -2,18 +2,23 @@ package pkg.ast;
 
 import pkg.Token;
 
+// The node class for the abstract syntax tree
 public abstract class Node {
 
+  // The token associated with this node
   private final Token token;
 
+  // The constructor for the Node class
   public Node(Token t){
     this.token = t;
   }
 
+  // Getter for the token
   public Token getToken(){
     return token;
   }
 
+  // Getter for the token literal value
   public String getTokenLiteral(){
     return token.getLiteral();
   }
